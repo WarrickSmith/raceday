1e1321dbc780380fa7a46e48873a620c39fe6384
Warrick Smith <warricksmith@developersinstitute.co.nz>
2022-01-04 12:38:19 +1300

    build: add react router v6 & basic page structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>RaceDay!</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
       </header>
     </div>
   );
---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>RaceDay Analysis</h2>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+export default Home;
