96d3898ac22f759cabc0e6261210a92367fb4ce7
Warrick Smith <warricksmith@developersinstitute.co.nz>
2022-01-13 14:56:57 +1300

    feat: add menu hamburger & css

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,43 +2,62 @@
 
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import Logo from "../logo.png";
 import LoginButton from "./LoginButton";
+import { MdClose } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
   console.log("Navbar Component Rendered!");
+
+  // Use useState and handleToggle to track hamburger open/close status
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
+  // Return the nav bar elements
   return (
     <nav className="navbar">
-      <div className="container">
-        <Link to="/" className="navbar-title">
-          <img src={Logo} alt="company logo" width="100" />
-        </Link>
-        <ul className="nav">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/raceday" className="nav-link">
-              Live Racing
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/racedata" className="nav-link">
-              Race Data
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <LoginButton />
-          </li>
-        </ul>
-      </div>
+      <Link to="/" className="navbar-logo">
+        <img src={Logo} alt="company logo" width="75" />
+      </Link>
+      <button className="btn-burger" onClick={handleToggle}>
+        {navbarOpen ? (
+          <MdClose className="burger" />
+        ) : (
+          <FiMenu className="burger" />
+        )}
+      </button>
+      <ul className={`menunav ${navbarOpen ? " showMenu" : ""}`}>
+        <li className="nav-item">
+          <Link to="/" className="nav-link" onClick={() => closeMenu()}>
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/raceday" className="nav-link" onClick={() => closeMenu()}>
+            Live Racing
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/racedata" className="nav-link" onClick={() => closeMenu()}>
+            Race Data
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link" onClick={() => closeMenu()}>
+            About
+          </Link>
+        </li>
+        <li className="nav-item" onClick={() => closeMenu()}>
+          <LoginButton />
+        </li>
+      </ul>
     </nav>
   );
 };
