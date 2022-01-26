cd0db961e6c7b25aa4a6f8ef5e8746597de7bff0
Warrick Smith <warricksmith@developersinstitute.co.nz>
2022-01-27 10:17:41 +1300

    test: remove unnecessary dependancy

--- client/src/components/RaceMeetings.test.js
+++ client/src/components/RaceMeetings.test.js
@@ -1,4 +1,4 @@
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import RaceMeetings from "./RaceMeetings";
 
