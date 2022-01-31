505d504a307139535eaffa7cd59b35d08da3ad0e
Warrick Smith <warricksmith@developersinstitute.co.nz>
2022-01-31 14:40:09 +1300

    fix: correct error with number string confusion

--- client/src/components/LiveRacing/RaceSelector.js
+++ client/src/components/LiveRacing/RaceSelector.js
@@ -4,43 +4,40 @@
 import "./RaceSelector.css";
 
 // Render Element
-const RaceSelector = ({ allRaces }) => {
+const RaceSelector = ({ allRaces, currentRace, setCurrentRace }) => {
   console.log(`RaceSelector Element Loading...`);
+
   // Event handler for selecting different race from dropdown list
   const handleOnChange = (event) => {
     console.log(`A change has been detected!`);
-    console.log(`event.target.value: `, event.target.value);
-    setCurrentRace(event.target.value);
+    console.log(`event.target.value: ${event.target.value}`);
+    // use parseInt to ensure numeric type is forced, otherwise may default to text
+    setCurrentRace(parseInt(event.target.value));
   };
 
   // Event handler for clicking on 'NEXT SCHEDULED RACE'
   const handleOnClick = () => {
     console.log(`A click event has been detected!`);
-    setCurrentRace(getNextRace(raceList));
+    // setCurrentRace(getNextRace(raceList));
   };
 
   // Event handler for clicking on '<<' decrease race by one
   const handleOnClickMinus = () => {
     if (currentRace > 0) {
-      setCurrentRace((prevCount) => prevCount - 1);
+      setCurrentRace((previousRace) => previousRace - 1);
     }
   };
 
   // Event handler for clicking on '>>' increase race by one
   const handleOnClickPlus = () => {
-    const numberOfRaces = raceList.length;
+    const numberOfRaces = allRaces.length;
     if (currentRace < numberOfRaces - 1) {
-      setCurrentRace((prevCount) => prevCount + 1);
+      setCurrentRace((previousRace) => previousRace + 1);
     }
   };
 
-  // Testing element functionailty
-  // console.log(
-  //   `Current Active Race: `,
-  //   currentRace,
-  //   `  |   Next Schedlued Race: `,
-  //   getNextRace(raceList)
-  // );
+  // Confirm current Race is being updates as appropriate
+  console.log(`Current Active Race: `, currentRace);
 
   return (
     <>
@@ -50,14 +47,12 @@ const RaceSelector = ({ allRaces }) => {
             className="race-select-textbox"
             id="raceList"
             name="raceList"
-            value={0}
-            // onChange={handleOnChange}
+            value={currentRace}
+            onChange={handleOnChange}
           >
             {allRaces.map((race, index) => (
-              <option
-                key={index + race.RaceName + race.RaceNumber}
-                value={index}
-              >
+              <option key={race.RaceStartTime + race.RaceName} value={index}>
+                {/* format date field to shorten width of rendered sttring */}
                 {`${new Date(race.RaceStartTime).toLocaleString("en-nz", {
                   month: "short",
                   day: "numeric",
@@ -73,17 +68,14 @@ const RaceSelector = ({ allRaces }) => {
         <div className="selector-buttons">
           <button
             className="button-minus"
-            // onClick={handleOnClickMinus}
+            onClick={handleOnClickMinus}
           >{`<<`}</button>
-          <button
-            className="button-next"
-            // onClick={handleOnClick}
-          >
+          <button className="button-next" onClick={handleOnClick}>
             NEXT SCHEDULED RACE
           </button>
           <button
             className="button-plus"
-            // onClick={handleOnClickPlus}
+            onClick={handleOnClickPlus}
           >{`>>`}</button>
         </div>
       </div>
