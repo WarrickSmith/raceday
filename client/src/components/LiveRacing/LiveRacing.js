6b5aa4078532317103b94cd9f3cf294df3006767
Warrick Smith <warricksmith@developersinstitute.co.nz>
2022-02-03 10:42:25 +1300

    fix: add err handling & set current race code

--- client/src/components/LiveRacing/LiveRacing.js
+++ client/src/components/LiveRacing/LiveRacing.js
@@ -1,6 +1,7 @@
 // This component will call the getAllRaces service and display a list of todays Race Meetings
 
 // Import dependancies
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import getAllRaces from "../../services/getAllRaces";
 import "./LiveRacing.css";
@@ -16,10 +17,21 @@ const LiveRacing = () => {
   // Create stateful variable to track currently selected Race
   const [currentRace, setCurrentRace] = useState(0);
 
-  // Run useEffect once to initially fetch allRaces data
+  // Run useEffect once to initially fetch allRaces data and set relevant variables
   useEffect(() => {
     const loadAllRaces = async () => {
       const result = await getAllRaces();
+      // find and set current race (currentRace) based on current local time
+      if (!result.length) {
+        setAllRaces("ERROR!");
+        return;
+      }
+      const dateNow = new Date();
+      const raceIndex = result.findIndex(
+        (race) => new Date(race.RaceStartTime) >= dateNow
+      );
+      // update variable states to trigger re-render of relevant elements
+      setCurrentRace(raceIndex);
       setAllRaces(result);
       return;
     };
@@ -27,10 +39,19 @@ const LiveRacing = () => {
   }, []);
 
   //Render and return LiveRacing element to application - Show loading animation if promise still pending (allRaces = undefined)
-  if (allRaces === undefined) {
+  if (allRaces === "ERROR!") {
     return (
       <div className="centertext">
-        <h2>The LiveRacing Element has rendered</h2>
+        <h2>Error fetching All Races for Today!</h2>
+        <Link to="/" className="nav-link">
+          <h3>'Click Me' to Show Meetings</h3>
+        </Link>
+      </div>
+    );
+  } else if (allRaces === undefined) {
+    return (
+      <div className="centertext">
+        <h2>The LiveRacing Element Rendering...</h2>
         <Spinner />
       </div>
     );
