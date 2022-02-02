02d9569f7b680578a18bbfdc98c820cf6f64e693
Warrick Smith <warricksmith@developersinstitute.co.nz>
2022-02-02 14:31:46 +1300

    fix: add racedata element logic and return data

--- client/src/components/LiveRacing/RaceData.js
+++ client/src/components/LiveRacing/RaceData.js
@@ -1,23 +1,56 @@
 // This component will return an Element to show race specific data for the race URL provided
 
 // Import dependancies
+import { useState, useEffect } from "react";
 import Spinner from "../Spinner";
+import getRace from "../../services/getRace";
 
 // Return RaceData element
 const RaceData = ({ raceUrl }) => {
-  console.log("RaceData Element Loading...", raceUrl);
-  if (raceUrl === undefined) {
+  console.log("RaceData Element Loading...");
+  // Create stateful variable to track current RaceData
+  const [raceData, setRaceData] = useState();
+
+  // Run useEffect once to initially fetch raceData by passing raceUrl to getRace()
+  useEffect(() => {
+    const loadRace = async () => {
+      const result = await getRace(raceUrl);
+      setRaceData(result);
+      return;
+    };
+    loadRace();
+  }, [raceUrl]);
+
+  // render a spinner until raceData is returned
+  if (raceData === undefined) {
     return (
       <div className="centertext">
         <h2>Awaiting RaceData...</h2>
+        <p>Current Race URL is: {raceUrl}</p>
         <Spinner />
       </div>
     );
-  } else
+  }
+  // Render raceData information for a specific race (raceUrl)
+  else
     return (
       <div className="racedata-container">
-        <h2>RaceData Element Loading...</h2>
-        <p>Current Race URL is: {raceUrl}</p>
+        <h2>RaceData:</h2>
+        <h3>Race Type: {raceData.meeting.raceType}</h3>
+        <p>Current Race number: {raceData.raceNumber} </p>
+        <p>Race Name: {raceData.raceName}</p>
+        <p>Race Distance: {raceData.raceDistance}</p>
+        <p>RUNNERS:</p>
+        <ol>
+          {raceData.runners.map((runner, index) => (
+            <li key={index + runner.runnerName}>
+              &nbsp;&nbsp;{" "}
+              <img alt="" src={runner.silkURL} width="20" height="20"></img>
+              &nbsp;&nbsp;
+              {runner.runnerName}
+            </li>
+          ))}
+        </ol>
       </div>
     );
 };
