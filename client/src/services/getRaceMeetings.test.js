1fea585e7db8a26a1f9e5bef121da238519080da
Warrick Smith <warricksmith@developersinstitute.co.nz>
2022-04-12 17:39:06 +1200

    fix: update test expect condition

--- client/src/services/getRaceMeetings.test.js
+++ client/src/services/getRaceMeetings.test.js
@@ -5,9 +5,12 @@ import getRaceMeetings from "./getRaceMeetings";
 describe("Unit Test for service-moduler getRaceMeetings", () => {
   // Test racemeetings object get returned from API server endpoint \racemeetings
 
+  jest.setTimeout(10000); // allow time for api server response
+
   it("Should fetch and return an object containing racemeetings data from API Servr \racemeetings", async () => {
     // test getRaces function returns an object with the 'meetingName' key to confirm data is present in the object returned.
     const received = await getRaceMeetings();
-    expect(received).toHaveProperty("meetings[0].meetingName");
+    expect(received).toHaveProperty("meetings");
+    expect(received.meetings[0]).toHaveProperty("meetingName");
   });
 });
