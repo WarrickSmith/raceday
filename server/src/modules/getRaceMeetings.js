8fef67b5f62c28e264bf5d78618caaa280908873
Warrick Smith <warrick.smith@neo.nz>
2024-04-29 08:56:31 +1200

    fix: update getRaceMeetings as backend AU TAB api looks to have changedjson structrue.

--- server/src/modules/getRaceMeetings.js
+++ server/src/modules/getRaceMeetings.js
@@ -1,34 +1,31 @@
 // Function to Connect to the Australian TAB API and query all race meetings for 'today'
 
-// REf URL to get active links for todays racing - https://api.beta.tab.com.au/v1/tab-info-service/racing/dates?jurisdiction=NSW
-// This will return an object with links for todays and the next few days race meetings.
-// Example - todays  meeting link will likely in this format: "meetings": "https://api.beta.tab.com.au/v1/tab-info-service/racing/dates/YYYY-MM-DD/meetings?jurisdiction=NSW"
+// REf URL to get active links for todays racing -  "https://api.beta.tab.com.au/v1/tab-info-service/racing/dates/YYYY-MM-DD/meetings?jurisdiction=NSW"
+// This will return an array with todays race meetings
 
-// Function to query 'all meeetings', then select a link to 'todays meetings'. Then fetch 'todays meeting' data and store in an object. Return the meetings object
-// Ref Meetings Overview: https://api.beta.tab.com.au/v1/tab-info-service/racing/dates?jurisdiction=NSW
+const getRaceMeetings = async (req, res, next) => {
+  const today = new Date()
+  const year = today.getFullYear()
+  const month = ('0' + (today.getMonth() + 1)).slice(-2)
+  const day = ('0' + today.getDate()).slice(-2)
+  const date = `${year}-${month}-${day}`
+  const jurisdiction = 'NSW'
 
-const fetch = require("node-fetch"); // Used to support asynch-await-fetch
-
-const getRaceMeetings = async (next) => {
   try {
-    // Fetch object containing links to meetings for 'today', 'tomorrow' etc
-    let result = await fetch(
-      "https://api.beta.tab.com.au/v1/tab-info-service/racing/dates?jurisdiction=NSW"
-    );
-    const RaceMeetings = await result.json();
-    const raceDay = RaceMeetings.dates[0]._links.meetings; // [0]=today, [1]=tomorrow, etc.
+    // Fetch object containing links to meetings for the specified date
+    const url = `https://api.beta.tab.com.au/v1/tab-info-service/racing/dates/${date}/meetings?jurisdiction=${jurisdiction}`
+    const response = await fetch(url)
 
-    // Fetch 'Today's' race meeting data object using raceDay as the fetch link
-    result = await fetch(raceDay, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const meetingsToday = await result.json();
-    return meetingsToday;
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
   } catch (err) {
-    next(err);
+    console.error(err.message)
+    throw err
   }
-};
+}
 
-module.exports = getRaceMeetings;
+module.exports = getRaceMeetings
